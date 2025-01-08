ba4616f416ce481f1f1fcf369539e924fbdf61e4
muheii <hsroot@asu.edu>
2025-01-07 17:39:07 -0700

    Update dialog design with previously unused information from API

--- src/lib/ContentDialog.svelte
+++ src/lib/ContentDialog.svelte
@@ -4,6 +4,8 @@
 	import VnControls from '../routes/vn/VnControls.svelte';
 	import { ContentType, type Content } from '../types/content';
 	import { buttonVariants } from './components/ui/button';
+    import { Badge, badgeVariants } from '$lib/components/ui/badge/index.js';
+    import { Calendar, Star, SquareArrowOutUpRight } from 'lucide-svelte';
 
     let { item }: { item: Content } = $props();
 
@@ -13,25 +15,50 @@
     <Dialog.Trigger class={buttonVariants({ variant: 'default' })}
         >Details</Dialog.Trigger
     >
-    <Dialog.Content class="h-[80vh] max-w-5xl overflow-hidden">
+    <Dialog.Content class="h-[80vh] max-w-5xl overflow-hidden p-6">
         <Dialog.Header >
-            <Dialog.Title>{item.title}</Dialog.Title>
-            <Dialog.Description>manage your content</Dialog.Description>
+            <div class="flex flex-col gap-y-2">
+                <div class="flex gap-x-2">
+                    <Dialog.Title class="text-2xl font-bold">{item.title}</Dialog.Title>
+                    <Dialog.Description class="text-lg mt-1">{item.title_japanese}</Dialog.Description>
+                </div>
+                <div class="flex gap-x-2">
+                    <Badge variant="secondary">{item.content_type}</Badge>
+                    {#if item.rating && item.votecount}
+                        <Badge variant="secondary" class="gap-x-1">
+                            <Star class="h-3.5 w-3.5 text-primary"/>
+                            <span>{item.rating} ({item.votecount.toLocaleString()})</span>
+                        </Badge>
+                    {/if}
+                    {#if item.release_date}
+                    <Badge variant="secondary" class="gap-x-1">
+                        <Calendar class="h-3.5 w-3.5 text-primary"/>
+                        <span>{item.release_date}</span>
+                    </Badge>
+                    {/if}
+                    <!--TODO: create <a> for external url with badgeVariants -->
+                </div>
+            </div>
         </Dialog.Header>
 
-        <div class="grid h-[65vh] grid-cols-2">
+        <div class="grid h-[65vh] grid-cols-2 pb-4 gap-x-6">
             <div class="w-full overflow-hidden rounded-lg">
                 <img class="h-full w-full object-cover" src={item.image_path} alt={item.title}>
             </div>
 
-            <div class="flex h-full flex-col gap-4 pl-4">
-                <div class="flex-1 overflow-y-auto">
-                    <p class="text-sm">{item.description || 'no description available'}</p>
+            <div class="flex h-full flex-col justify-between">
+                <div class="flex flex-col">
+                    <h3 class="font-semibold mb-2">Description</h3>
+                    <!--TODO: create better looking scrollbar, consider removing tailwind-scrollbar-->
+                    <div class="h-72 scrollbar-thin overflow-y-auto">
+                        <p class="text-sm text-muted-foreground">{item.description || 'No description available.'}</p>
+                    </div>
                 </div>
-
-                <div>
+                <div class="flex-none pt-6 border-t border-border">
                     {#if item.content_type === ContentType.Anime}
-                        <AnimeControls anime={item}></AnimeControls>
+                        <div class="max-h-32 overflow-y-auto">
+                            <AnimeControls anime={item}></AnimeControls>
+                        </div>
                     {:else}
                         <VnControls vn={item}></VnControls>
                     {/if}
--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -9,7 +9,7 @@
 <div class="flex gap-2">
     <Input type="text" bind:value={query} placeholder="Search for content..." class="w-1/3"></Input>
     <Select.Root type="single" name="selectedContent" bind:value={contentType}>
-        <Select.Trigger class="w-1/6"></Select.Trigger>
+        <Select.Trigger class="w-1/6">{contentType}</Select.Trigger>
         <Select.Content>
             <Select.Item value={ContentType.Anime} label="Anime" />
             <Select.Item value={ContentType.Vn} label="Vn" />
